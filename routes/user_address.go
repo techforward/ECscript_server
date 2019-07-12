cd514b227206582f0de5416b5c20a5ed5b7588cc
ErgoFriend <erg.friend@gmail.com>
2019-07-12 23:29:44 +0900

    UserAddressRouter

--- routes/user_address.go
+++ routes/user_address.go
@@ -4,13 +4,12 @@ import (
 	"github.com/techforward/ECscript_server/handler"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 // UserAddressRouter User's Address Router
 func UserAddressRouter(e *echo.Echo) {
 	loginRequired := e.Group("/user_address")
-	loginRequired.Use(middleware.JWT([]byte("secret")))
+	// loginRequired.Use(middleware.JWT([]byte("secret")))
 
 	loginRequired.GET("/", handler.GetAllUserAddresses)
 	loginRequired.GET("/:id", handler.GetUserAddress)
